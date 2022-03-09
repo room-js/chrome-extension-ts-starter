c40139eb57967341d585d8632507c15c23080945
Yuri Drabik <yurist38@mail.ru>
2022-03-10 00:31:14 +0700

    Update deps (#17)

--- src/devtools/app/App.tsx
+++ src/devtools/app/App.tsx
@@ -2,7 +2,9 @@ import React, { FC } from 'react';
 import { Container } from './styles';
 
 const App: FC = () => (
-  <Container>Hey, it is the DevTools app running on React.js</Container>
+  <Container>
+    Hey, it is the DevTools app running on React.js
+  </Container>
 );
 
 export default App;
