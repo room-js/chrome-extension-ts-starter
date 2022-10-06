14f42afe75a3bee7c6bdeeb93cd57acae86916ce
Yuri Drabik <yurist38@gmail.com>
2022-10-06 12:47:16 +0200

    Added axios example and updated dependencies

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,15 @@
+import axios from 'axios';
+import fetchAdapter from '@vespaiach/axios-fetch-adapter';
+
 console.log('Hey! This code is executed in the background, you will not see it in the browser console...');
+
+// example of using Axios in the background script. You need to use the custom adapter.
+axios
+  .request({
+    url: 'https://google.com',
+    method: 'GET',
+    adapter: fetchAdapter,
+  })
+  .then((data) => {
+    console.log('DATA:', data);
+  });
